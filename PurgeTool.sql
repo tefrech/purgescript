bf6f69224a66831c03f4e6a49aa558cb44898af9
tefrech <75037841+tefrech@users.noreply.github.com>
2022-06-13 19:32:14 -0700

    Update PurgeTool.sql
    Clarity and clean up

--- PurgeTool.sql
+++ PurgeTool.sql
@@ -1,158 +1,143 @@
+-- Written by Todd Frech, 2021 in MySQL, originally Microsoft Access
 -- GOAL OF THIS TOOL:
--- Isolate customer records who had vacated ALL of their leases prior to the Cut Off date 
--- and purge all related transaction records
+-- 1. Isolate customer records that had vacated all of their spaces prior to the Cut Off Date and purge all related transaction records
+-- 2. Do not affect any financial reports for the time range following the Cut Off Date
+-- ITEMS TO CONSIDER
+-- 1. Accounts can record transactions following their End Date, so those must be identified and filtered out
+-- 2. Accounts must have had all associated spaces vacated on the Cut Off Date
+-- 3. 
 
-USE dam;
+
 CREATE TABLE PurgeParameters (SiteID Text(3), CutOff Date);
-DROP TABLE CustAcctPurgeFinalReport;
-CREATE TABLE CustAcctPurgeFinalReport (
+
+CREATE TABLE AccountsToPurgeFinalReport (
 	SiteID Text
-    , CustomerAccount Int
-    , CustomerFirstName Text
-    , CustomerLastName Text
-    , UnitMask Text
-    , CustomerUnitMoveInDate Date
-    , CustomerUnitMoveOutDate Date
+    , Account Int
+    , FirstName Text
+    , LastName Text
+    , Space Text
+    , StartDate Date
+    , EndDate Date
     );
 CREATE TABLE ResultsCheck (
 	SiteID Text
-    , CustomerAccount Int
-    , CustomerFirstName Text
-    , CustomerLastName Text
-    , UnitMask Text
-    , CustomerUnitStatus Text
-    , CustomerUnitMoveInDate Date
-    , CustomerUnitMoveOutDate Date
+    , Account Int
+    , FirstName Text
+    , LastName Text
+    , Space Text
+    , CustAccountsStatus Text
+    , StartDate Date
+    , EndDate Date
     , PurgeStatus Text
     , Qualify Text
     );
 
-DELETE FROM CustAcctPurgeFinalReport;
+-- Building the report of accounts to be purged
 
-INSERT INTO PurgeParameters ( SiteID, CutOff )
-VALUES ('XXL', '2016-01-01');
-
-SELECT * FROM purgeparameters;
-
--- Building the report of who''s going to be purged
-
-INSERT INTO CustAcctPurgeFinalReport
+INSERT INTO AccountsToPurgeFinalReport
 WITH 
-	-- Select all vacant leases
-	CustAcctVacant AS (
-		SELECT SiteID, CustomerAccount
-		FROM CustomerUnits
-		WHERE CustomerUnitStatus = 2 
-        AND CustomerUnits.SiteID IN (SELECT SiteID FROM PurgeParameters) 
-        AND CustomerUnitMoveOutDate < ANY (SELECT CutOff FROM PurgeParameters))
-	-- Select all leases vacated after the CutOff
-	, CustAcctVacantAfter AS (
-		SELECT SiteID, CustomerAccount
-		FROM CustomerUnits
-		WHERE CustomerUnitStatus = 2 
-        AND CustomerUnits.SiteID IN (SELECT SiteID FROM PurgeParameters) 
-        AND CustomerUnitMoveOutDate >= ANY (SELECT CutOff FROM PurgeParameters))
-	-- Select all occupied leases
-	, CustAcctOccupied AS (
-		SELECT CustomerUnits.SiteID, CustomerUnits.CustomerAccount
-		FROM CustomerUnits
-		WHERE CustomerUnits.CustomerUnitStatus IN (1, 5, 6) 
-        AND CustomerUnits.SiteID IN (SELECT SiteID FROM PurgeParameters))
-	-- Join CustAcctVacant with CustAcctOccupied and only keep the customers that do NOT exist in CustAcctOccupied
-	, AcctFiltered AS (
-		SELECT DISTINCT CustAcctVacant.CustomerAccount, CustAcctVacant.SiteID 
-		FROM CustAcctVacant 
-        LEFT JOIN CustAcctOccupied 
-			ON CustAcctVacant.CustomerAccount = CustAcctOccupied.CustomerAccount
-            AND CustAcctVacant.SiteID = CustAcctOccupied.SiteID
-		WHERE CustAcctOccupied.CustomerAccount IS NULL
-        	AND CustAcctOccupied.SiteID IS NULL)
-	-- Join AcctFiltered with CustAcctVacantAfter and keep the customers that do NOT exist in CustAcctVacantAfter
-	, CustAcctPurge AS (
-		SELECT DISTINCT AcctFiltered.CustomerAccount, AcctFiltered.SiteID
-		FROM AcctFiltered 
-		LEFT JOIN CustAcctVacantAfter 
-			ON AcctFiltered.SiteID = CustAcctVacantAfter.SiteID
-			AND AcctFiltered.CustomerAccount = CustAcctVacantAfter.CustomerAccount
-		WHERE CustAcctVacantAfter.CustomerAccount IS NULL 
-			AND CustAcctVacantAfter.SiteID IS NULL)
-	-- Select all customer records that registered a "Vacant Unit Income," "Reversal," or "Uncollected" transaction AFTER the CutOff
+	-- Select all vacant accounts
+	VacantAccounts AS (
+		SELECT SiteID, Account
+		FROM CustAccounts
+		WHERE CustAccountsStatus = 2 
+        AND CustAccounts.SiteID IN (SELECT SiteID FROM PurgeParameters) 
+        AND EndDate < ANY (SELECT CutOff FROM PurgeParameters))
+	-- Select all accounts vacated after the CutOffDate
+	, VacantAccountsAfter AS (
+		SELECT SiteID, Account
+		FROM CustAccounts
+		WHERE CustAccountsStatus = 2 
+        AND CustAccounts.SiteID IN (SELECT SiteID FROM PurgeParameters) 
+        AND EndDate >= ANY (SELECT CutOff FROM PurgeParameters))
+	-- Select all active accounts
+	, ActiveAccounts AS (
+		SELECT CustAccounts.SiteID, CustAccounts.Account
+		FROM CustAccounts
+		WHERE CustAccounts.CustAccountsStatus IN (1, 5, 6) 
+        AND CustAccounts.SiteID IN (SELECT SiteID FROM PurgeParameters))
+	-- Join VacantAccounts with ActiveAccounts and only keep the accounts that do NOT exist in ActiveAccounts
+	, AccountsFiltered AS (
+		SELECT DISTINCT VacantAccounts.Account, VacantAccounts.SiteID 
+		FROM VacantAccounts 
+        LEFT JOIN ActiveAccounts 
+			ON VacantAccounts.Account = ActiveAccounts.Account
+            AND VacantAccounts.SiteID = ActiveAccounts.SiteID
+		WHERE ActiveAccounts.Account IS NULL
+        	AND ActiveAccounts.SiteID IS NULL)
+	-- Join AccountsFiltered with VacantAccountsAfter and keep the accounts that do NOT exist in VacantAccountsAfter
+	, AccountsToPurge AS (
+		SELECT DISTINCT AccountsFiltered.Account, AccountsFiltered.SiteID
+		FROM AccountsFiltered 
+		LEFT JOIN VacantAccountsAfter 
+			ON AccountsFiltered.SiteID = VacantAccountsAfter.SiteID
+			AND AccountsFiltered.Account = VacantAccountsAfter.Account
+		WHERE VacantAccountsAfter.Account IS NULL 
+			AND VacantAccountsAfter.SiteID IS NULL)
+	-- Select all accounts that registered a "Vacant Income," "Reversal," or "Uncollected" transaction AFTER the Cut Off Date
 	, VUIAccounts AS (
-		SELECT DISTINCT CustomerAccount, SiteID
-		FROM Transactions INNER JOIN TransactionDetail ON Transactions.TransactionID=TransactionDetail.TransactionID
-		WHERE TransactionDetail.TransactionCode IN (52,122,57) 
-			AND Transactions.SiteID IN (SELECT SiteID FROM PurgeParameters)  
-			AND TransactionDetail.TransactionFromDate >= (SELECT CutOff FROM PurgeParameters))
-	-- Join CustAcctPurge with VUIAccounts and keep the customers that do NOT exist in VUIAccounts
-	, CustAcctPurgeFinal AS (
-		SELECT DISTINCT CustAcctPurge.CustomerAccount, CustAcctPurge.SiteID
-		FROM CustAcctPurge 
-		LEFT JOIN VUIAccounts ON CustAcctPurge.CustomerAccount = VUIAccounts.CustomerAccount
-			AND (CustAcctPurge.SiteID = VUIAccounts.SiteID)
-		WHERE VUIAccounts.CustomerAccount IS NULL AND VUIAccounts.SiteID IS NULL)
+		SELECT DISTINCT Account, SiteID
+		FROM Trax INNER JOIN TraxDetail ON Trax.TraxID = TraxDetail.TraxID
+		WHERE TraxDetail.TraxCode IN (52,122,57) 
+			AND Trax.SiteID IN (SELECT SiteID FROM PurgeParameters)  
+			AND TraxDetail.TraxFromDate >= (SELECT CutOff FROM PurgeParameters))
+	-- Join AccountsToPurge with VUIAccounts and keep the accounts that do NOT exist in VUIAccounts
+	, AccountsToPurgeFinal AS (
+		SELECT DISTINCT AccountsToPurge.Account, AccountsToPurge.SiteID
+		FROM AccountsToPurge 
+		LEFT JOIN VUIAccounts ON AccountsToPurge.Account = VUIAccounts.Account
+			AND (AccountsToPurge.SiteID = VUIAccounts.SiteID)
+		WHERE VUIAccounts.Account IS NULL AND VUIAccounts.SiteID IS NULL)
 --Start to build the output
-SELECT Customers.SiteID
-	, Customers.CustomerAccount AS Acct
-    , Customers.CustomerFirstName AS First_Name
-    , Customers.CustomerLastName AS Last_Name
-    , Units.UnitMask AS Unit
-    , CustomerUnits.CustomerUnitMoveInDate AS Move_In_Date
-    , CustomerUnits.CustomerUnitMoveOutDate AS Move_Out_Date
-FROM Customers 
-INNER JOIN (CustomerUnits INNER JOIN Units ON CustomerUnits.UnitID = Units.UnitID) 
-	ON Customers.CustomerAccount=CustomerUnits.CustomerAccount
-	AND Customers.SiteID=CustomerUnits.SiteID
-JOIN CustAcctPurgeFinal
-	ON CustomerUnits.CustomerAccount=CustAcctPurgeFinal.CustomerAccount
-	AND Customers.CustomerAccount=CustAcctPurgeFinal.CustomerAccount
--- Adding the category description, as users do not know the IDs
-INNER JOIN lookupunitstatus 
-	ON CustomerUnits.CustomerUnitStatus = lookupunitstatus.UnitStatusID
-WHERE  Customers.SiteID IN 
+SELECT Accounts.SiteID
+	, Accounts.Account
+    , Accounts.FirstName
+    , Accounts.LastName
+    , Spaces.Space
+    , CustAccounts.StartDate
+    , CustAccounts.EndDate
+FROM Accounts 
+JOIN (CustAccounts INNER JOIN Spaces ON CustAccounts.ID = Spaces.ID) 
+	ON Accounts.Account=CustAccounts.Account
+	AND Accounts.SiteID=CustAccounts.SiteID
+JOIN AccountsToPurgeFinal
+	ON CustAccounts.Account=AccountsToPurgeFinal.Account
+	AND Accounts.Account=AccountsToPurgeFinal.Account
+JOIN LookupStatus 
+	ON CustAccounts.CustAccountsStatus = LookupStatus.StatusID
+WHERE  Accounts.SiteID IN 
 	(SELECT SiteID FROM PurgeParameters)
-AND Customers.CustomerAccount IN 
-	(SELECT CustomerAccount FROM CustAcctPurgeFinal)
-ORDER BY Customers.CustomerAccount, Units.UnitMask
-;
-
-SELECT * FROM CustAcctpurgeFinalReport;
+AND Accounts.Account IN 
+	(SELECT Account FROM AccountsToPurgeFinal)
+ORDER BY Accounts.Account, Spaces.Space;
 
 
--- INSERT INTO ResultsCheck
-SELECT Customers.SiteID
-    , Customers.CustomerAccount
-    , Customers.CustomerFirstName
-    , Customers.CustomerLastName
-    , Units.UnitMask AS Unit
-    , lookupunitstatus.UnitStatusName AS Status
-    , CustomerUnits.CustomerUnitMoveInDate
-    , CustomerUnits.CustomerUnitMoveOutDate
-    , IF(Customers.CustomerAccount IN 
-    	(SELECT CustomerAccount FROM CustAcctPurgeFinalReport), 'PURGE', 'KEEP') 
+
+SELECT Accounts.SiteID
+    , Accounts.Account
+    , Accounts.FirstName
+    , Accounts.LastName
+    , Spaces.Space AS Unit
+    , LookupStatus.Name AS Status
+    , CustAccounts.StartDate
+    , CustAccounts.EndDate
+    , IF(Accounts.Account IN 
+    	(SELECT Account FROM AccountsToPurgeFinalReport), 'PURGE', 'KEEP') 
     	AS PurgeStatus
     , IF(
-    	(CustomerUnits.CustomerUnitMoveOutDate < (SELECT CutOff FROM PurgeParameters)) 
-    	AND CustomerUnits.CustomerUnitStatus = 2, 'Yes', 'No') 
+    	(CustAccounts.EndDate < (SELECT CutOff FROM PurgeParameters) )
+    	AND CustAccounts.CustAccountsStatus = 2, 'Yes', 'No') 
     	AS Qualify
-FROM Customers 
-INNER JOIN CustomerUnits
-	ON Customers.CustomerAccount = CustomerUnits.CustomerAccount
-	AND Customers.SiteID = CustomerUnits.SiteID
-INNER JOIN Units 
-	ON CustomerUnits.UnitID = Units.UnitID
-LEFT JOIN CustAcctPurgeFinalReport
-	ON CustomerUnits.CustomerAccount = CustAcctPurgeFinalReport.CustomerAccount
-	AND Customers.CustomerAccount = CustAcctPurgeFinalReport.CustomerAccount
-    AND Units.UnitMask = custacctpurgefinalreport.unitmask
-INNER JOIN lookupunitstatus 
-	ON CustomerUnits.CustomerUnitStatus = lookupunitstatus.UnitStatusID
-#WHERE  Customers.SiteID IN 
-#	(SELECT SiteID FROM PurgeParameters)
-#AND Customers.CustomerAccount IN 
-#	(SELECT CustomerAccount FROM CustAcctPurgeFinalReport)
-ORDER BY Customers.CustomerAccount, Units.UnitMask
-;
-DELETE FROM resultscheck;
-SELECT * FROM resultscheck;
-
-ALTER TABLE units DROP COLUMN CustomerAccount;
+FROM Accounts 
+JOIN CustAccounts
+	ON Accounts.Account = CustAccounts.Account
+	AND Accounts.SiteID = CustAccounts.SiteID
+JOIN Spaces 
+	ON CustAccounts.ID = Spaces.ID
+LEFT JOIN AccountsToPurgeFinalReport
+	ON CustAccounts.Account = AccountsToPurgeFinalReport.Account
+	AND Accounts.Account = AccountsToPurgeFinalReport.Account
+    AND Spaces.Space = AccountsToPurgeFinalReport.Space
+JOIN LookupStatus 
+	ON CustAccounts.CustAccountsStatus = LookupStatus.ID
+ORDER BY Accounts.Account, Spaces.Space;
